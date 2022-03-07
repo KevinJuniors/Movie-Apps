cf5c5d375a19e5f4e533672866197cff955e6373
DESKTOP-U172KAU\home <su960618@gmail.com>
2022-03-07 16:04:47 +0900

    [2022-03-07] Vanilla.js Click Event 예제 업로드

---
+++ js/index.js
@@ -0,0 +1,8 @@
+const button = document.getElementById("btn");
+// const button = document.querySelector("#btn");
+        
+function handleClick() {
+    console.log("버튼을 클릭 했습니다.");
+}
+
+button.addEventListener("click", handleClick);
