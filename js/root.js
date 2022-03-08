73ae2576f73c576f06ca629f2afeff11022f0fe7
minsm0618 <su960618@gmail.com>
2022-03-09 02:02:02 +0900

    [2022-03-08] JSX

--- js/root.js
+++ js/root.js
@@ -1,8 +1,8 @@
-const button = document.getElementById("btn");
 // const button = document.querySelector("#btn");
-        
+const button = document.getElementById("btn");
+
 function handleClick() {
-    console.log("버튼을 클릭 했습니다.");
+    console.log("버튼을 클릭하셨습니다.");
 }
 
 button.addEventListener("click", handleClick);
