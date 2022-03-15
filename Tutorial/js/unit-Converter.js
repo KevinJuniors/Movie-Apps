65c32bcba18023d1d6d99fe5af6eb49130c47f8c
minsm0618 <su960618@gmail.com>
2022-03-16 02:51:29 +0900

    [2022-03-15] React - useStact를 활용한 시간 단위 변환기 구현 완성

--- Tutorial/js/unit-Converter.js
+++ Tutorial/js/unit-Converter.js
@@ -1,24 +1,47 @@
 function App() {
-    const [minutes, setMinutes] = React.useState();
+    const [times, setTimes] = React.useState(0);
+    const [flipped, setFlipped] = React.useState(false);
     const onChange = (event) => {
-        setMinutes(event.target.value);
+        setTimes(event.target.value);
+    }
+    
+    const reset = () => setTimes(0);
+    const onFlip = () => {
+        reset();
+        setFlipped(current => !current);
     }
 
     return (
         <div>
             <h1 className="Text">시간 단위변환기</h1>
+            
+            <div>
+                <input 
+                    type="number" 
+                    id="Minutes" 
+                    placeholder="Minute" 
+                    value={flipped ? times * 60 : times}
+                    disabled={flipped === true}
+                    onChange={onChange}
+                />
+                
+                <label htmlFor="Minutes">분 은</label>
+            </div>
 
-            <input 
-                type="number" 
-                id="Minutes" 
-                placeholder="Minute" 
-                value={minutes} 
-                onChange={onChange} 
-            />
-                <label htmlFor="Minutes">분</label> →
-            <h4>변환하고 싶은 시간 단위의 결과는 : {minutes} 분</h4>
-            <input type="number" id="Hours" placeholder="Hour" />
-                <label htmlFor="Hours">시</label>
+            <div>
+                <input 
+                    type="number" 
+                    id="Hours" 
+                    placeholder="Hour"
+                    value={flipped ? times : Math.round(times / 60)}
+                    disabled={flipped === false}
+                />
+
+                <label htmlFor="Hours">시간 입니다.</label>
+            </div>
+
+            <button onClick={reset}>Reset</button>
+            <button onClick={onFlip}>Flip</button>
         </div>
     )
 }
